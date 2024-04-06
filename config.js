df586987d9fa955c4dbb76b4cfaeddde79e4092c
ZekromDev <108199641+ZekromDev@users.noreply.github.com>
2024-04-07 01:08:23 +0200

    Update

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    token: "MTIwNzk1NzMwODk0MTAxNzE0OA.GmqcoX.KlC05kLr_bmof08m5L6sBtRKWe8cb76x_RqpwQ",
+    token: ".",
     prefix: "+",
     allowedUserIDs: ['1207957308941017148', 'yourid2', 'yourid3']
 };
