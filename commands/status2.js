22869de6e24d443d9b861cfe1ec8e8c490c34f49
DEVROCK <114628634+devrock07@users.noreply.github.com>
2024-04-28 10:31:11 +0530

    Update status2.js

--- commands/status2.js
+++ commands/status2.js
@@ -6,21 +6,42 @@ module.exports = {
     execute(channel, message, client, args) {
         console.clear();
         console.log(`${client.user.tag} - rich presence started!`);
+        
+        // Send a message in the same channel where the command was used, confirming that the rich presence has started
+        message.channel.send('Rich Presence started.').catch(console.error);
 
-        const r = new Discord.RichPresence()
-            .setType('PLAYING')
-            .setURL('https://www.twitch.tv/zenithsenpai07')
-            .setState('bye afk <3')
-            .setName('<3')
-            .setDetails('Details')
-            .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1224303568794812501/1224602723496890419/a_5bc2ea8afd50a19ab18380ccd1d54a12.gif?ex=661e1729&is=660ba229&hm=0eb9d88ffac0a2fbda6ed7ea46d630e14ac87a0b4cd65c73b554218a24296d3c&')
-            .setAssetsLargeText('Cncs')
-            .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1224303568794812501/1224602723496890419/a_5bc2ea8afd50a19ab18380ccd1d54a12.gif?ex=661e1729&is=660ba229&hm=0eb9d88ffac0a2fbda6ed7ea46d630e14ac87a0b4cd65c73b554218a24296d3c&')
-            .setAssetsSmallText('Arozex')
-            .addButton('Cncs', 'https://discord.gg/cncs')
-            .addButton('Join Arozex Development', 'https://discord.gg/hrWB2MaEu8');
+        // Check if the first argument is "streaming"
+        if (args[0] && args[0].toLowerCase() === 'streaming') {
+            // Check if enough arguments are provided for streaming status
+            if (args.length < 2) {
+                return message.channel.send('Please provide the streaming URL and status message. Usage: !status2 streaming <URL> <message>').catch(console.error);
+            }
+            
+            const url = args[1]; // Streaming URL
+            const statusMessage = args.slice(2).join(' '); // Status message
 
-        client.user.setActivity(r);
-        client.user.setPresence({ status: "dnd" });
+            // Set the streaming status
+            client.user.setActivity(statusMessage, { type: 'STREAMING', url: url });
+            message.channel.send(`Streaming status set to "${statusMessage}"`).catch(console.error);
+        } else {
+            // Set the default custom status using Rich Presence
+            const r = new Discord.RichPresence()
+                .setType('STREAMING')
+                .setURL('https://www.twitch.tv/zenithsenpai07')
+                .setState('bye afk <3')
+                .setName('<3')
+                .setDetails('Details')
+                .setAssetsLargeImage('https://cdn.discordapp.com/avatars/1210993586187796510/a_adfe2482217fe513c7000a3f77b79bc1.gif?size=1024')
+                .setAssetsLargeText('add1')
+                .setAssetsSmallImage('https://cdn.discordapp.com/avatars/1210993586187796510/a_adfe2482217fe513c7000a3f77b79bc1.gif?size=1024')
+                .setAssetsSmallText('add2')
+                .addButton('add1', 'https://discord.gg/5NmRywHM')
+                .addButton('add2', 'https://discord.gg/5NmRywHM');
+    
+            client.user.setActivity(r);
+        }
+        
+        // Set presence status to "streaming"
+        client.user.setPresence({ status: "streaming" });
     }
 };
